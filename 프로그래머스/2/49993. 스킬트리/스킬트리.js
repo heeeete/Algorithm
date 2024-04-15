e381f77fafb93a6765a0d04a1d476f57ea880ce8
heeeete <101648575+heeeete@users.noreply.github.com>
2024-04-15 16:53:00 +0900

    [level 2] Title: 스킬트리, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.js
@@ -1,27 +1,12 @@
 function solution(skill, skill_trees) {
-	let answer = 0;
-	const obj = {};
-	const arr = skill.split("");
-
-	for (let i = 0; i < skill_trees.length; i++) {
-		let flag = 1;
-		for (let s of arr) {
-			obj[s] = -1;
+	function filters(skills) {
+		const temp = skill.split("");
+		for (let i = 0; i < skills.length; i++) {
+			if (!skill.includes(skills[i])) continue;
+			if (skills[i] === temp.shift()) continue;
+			return false;
 		}
-		for (let j = 0; j < skill_trees[i].length; j++) {
-			if (obj[skill_trees[i][j]]) {
-				let idx = arr.indexOf(skill_trees[i][j]);
-				if (idx === 0) obj[arr[idx]] = 1;
-				else {
-					idx--;
-					if (obj[arr[idx]] === -1) {
-						flag = 0;
-						break;
-					} else obj[arr[++idx]] = 1;
-				}
-			}
-		}
-		if (flag) answer++;
+		return true;
 	}
-	return answer;
+	return skill_trees.filter(filters).length;
 }
