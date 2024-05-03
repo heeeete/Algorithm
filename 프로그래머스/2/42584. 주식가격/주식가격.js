f7eb708cc0605cb0dfd52d421fb7dee62893151f
heeeete <101648575+heeeete@users.noreply.github.com>
2024-05-03 14:10:24 +0900

    [level 2] Title: 주식가격, Time: 31.97 ms, Memory: 41.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -0,0 +1,18 @@
+function solution(prices) {
+	var answer = [];
+
+	for (let i = 0; i < prices.length; i++) {
+		let time = 0;
+		for (let j = i + 1; j < prices.length; j++) {
+			if (prices[i] <= prices[j]) {
+				time++;
+			} else {
+				time++;
+				break;
+			}
+		}
+		answer.push(time);
+	}
+
+	return answer;
+}
