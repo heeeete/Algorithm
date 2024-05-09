8b4a91eb9f475fb863d93eee830aa6f0907a7c41
heeeete <101648575+heeeete@users.noreply.github.com>
2024-05-09 13:03:55 +0900

    [level 3] Title: 순위, Time: 20.59 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.js
+++ 프로그래머스/3/49191. 순위/순위.js
@@ -1,77 +1,35 @@
 function solution(n, results) {
 	var answer = 0;
-	const winners = Array.from(Array(n + 1), () => []);
-	const losers = Array.from(Array(n + 1), () => []);
-	const ranking = Array.from(Array(n + 1), () => 0);
+	const graph = Array.from(Array(n), () => Array(n).fill(-Infinity));
+	for (let [winner, loser] of results) {
+		graph[winner - 1][loser - 1] = 1;
+		graph[loser - 1][winner - 1] = -1;
+	}
+	for (let i = 0; i < n; i++) graph[i][i] = 0;
 
-	for (let [win, lose] of results) {
-		winners[win].push(lose);
-		losers[lose].push(win);
+	//i = 경유지
+	//j = 출발지
+	//k = 목적지
+	for (let i = 0; i < n; i++) {
+		for (let j = 0; j < n; j++) {
+			for (let k = 0; k < n; k++) {
+				if (graph[j][k] === -Infinity && graph[j][i] !== -Infinity) {
+					if (graph[j][i] === 1 && graph[i][k] === 1) graph[j][k] = 1;
+					else if (graph[j][i] === -1 && graph[i][k] === -1) graph[j][k] = -1;
+				}
+			}
+		}
 	}
 
-	// console.log(winners);
-	// console.log(losers);
-
-	for (let i = 1; i < n + 1; i++) {
-		const winNodes = winners[i];
-		const loseNodes = losers[i];
-		const winVisited = Array.from(Array(n + 1), () => 0);
-		const loseVisited = Array.from(Array(n + 1), () => 0);
-		winVisited[i] = 1;
-		loseVisited[i] = 1;
-		let prevLen = 0;
-		// console.log("-------------win");
-		while (1) {
-			let len = winNodes.length;
-			for (let i = prevLen; i < len; i++) {
-				const idx = winNodes[i];
-				if (winVisited[idx] === 0) {
-					winVisited[idx] = 1;
-					for (let j = 0; j < winners[idx].length; j++)
-						if (
-							winVisited[winners[idx][j]] === 0 &&
-							winNodes.indexOf(winners[idx][j]) === -1
-						)
-							winNodes.push(winners[idx][j]);
-				}
-			}
-			if (prevLen === len) break;
-			prevLen = len;
-		}
-		prevLen = 0;
-		// console.log(winNodes);
-		// console.log("-------------LOSE");
-		while (1) {
-			let len = loseNodes.length;
-			for (let i = prevLen; i < len; i++) {
-				const idx = loseNodes[i];
-				if (loseVisited[idx] === 0) {
-					loseVisited[idx] = 1;
-					for (let j = 0; j < losers[idx].length; j++)
-						if (
-							loseVisited[losers[idx][j]] === 0 &&
-							loseNodes.indexOf(losers[idx][j]) === -1
-						)
-							loseNodes.push(losers[idx][j]);
-				}
-			}
-			if (prevLen === len) break;
-			prevLen = len;
-		}
-		// console.log("WIN = ", winNodes);
-		// console.log("LOSE = ", loseNodes);
-		// console.log(winVisited);
-		// console.log(loseVisited);
+	for (let player of graph) {
 		let flag = 1;
-		for (let i = 1; i < winVisited.length; i++) {
-			if (!winVisited[i] && !loseVisited[i]) {
+		for (let result of player) {
+			if (result === -Infinity) {
 				flag = 0;
 				break;
 			}
 		}
-		if (flag) {
-			answer++;
-		}
+		if (flag) answer++;
 	}
 
 	return answer;
