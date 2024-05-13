13a1d5d9816a86f5ed6fd8001c9af41eefb18492
heeeete <101648575+heeeete@users.noreply.github.com>
2024-05-13 15:59:45 +0900

    [level 3] Title: 양과 늑대, Time: 1.11 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.js
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.js
@@ -6,17 +6,15 @@ function solution(info, edges) {
 	for (let [parent, child] of edges) nodeList[parent].push(child);
 
 	function dfs(nodes, sheep, wolf) {
-		if (sheep <= wolf) {
-			return answer.push(sheep);
-		}
-
-		for (let node of nodes) {
-			if (!visited[node]) {
-				visited[node] = 1;
-				if (info[node] === 0)
-					dfs([...nodes, ...nodeList[node]], sheep + 1, wolf);
-				else dfs([...nodes, ...nodeList[node]], sheep, wolf + 1);
-				visited[node] = 0;
+		if (sheep > wolf) {
+			for (let node of nodes) {
+				if (!visited[node]) {
+					visited[node] = 1;
+					if (info[node] === 0)
+						dfs([...nodes, ...nodeList[node]], sheep + 1, wolf);
+					else dfs([...nodes, ...nodeList[node]], sheep, wolf + 1);
+					visited[node] = 0;
+				}
 			}
 		}
 		answer.push(sheep);
