b3b7e2d931c27c6fba53652ac49e9df4545b0984
heeeete <101648575+heeeete@users.noreply.github.com>
2024-05-17 14:58:42 +0900

    [level 3] Title: 양과 늑대, Time: 0.73 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.js
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.js
@@ -1,25 +1,24 @@
 function solution(info, edges) {
 	const answer = [];
-	const nodeList = Array.from(Array(info.length), () => []);
+	const nodes = Array.from(Array(info.length), () => []);
 	const visited = {};
+	for (let [s, e] of edges) nodes[s].push(e);
 
-	for (let [parent, child] of edges) nodeList[parent].push(child);
-
-	function dfs(nodes, sheep, wolf) {
+	function dfs(arr, sheep, wolf) {
 		if (sheep > wolf) {
-			for (let node of nodes) {
+			for (let node of arr) {
 				if (!visited[node]) {
 					visited[node] = 1;
-					if (info[node] === 0)
-						dfs([...nodes, ...nodeList[node]], sheep + 1, wolf);
-					else dfs([...nodes, ...nodeList[node]], sheep, wolf + 1);
+					if (info[node] === 0) dfs([...arr, ...nodes[node]], sheep + 1, wolf);
+					else dfs([...arr, ...nodes[node]], sheep, wolf + 1);
 					visited[node] = 0;
 				}
 			}
+			answer.push(sheep);
 		}
-		answer.push(sheep);
 	}
 
-	dfs([...nodeList[0]], 1, 0);
+	dfs([...nodes[0]], 1, 0);
+
 	return Math.max(...answer);
 }
