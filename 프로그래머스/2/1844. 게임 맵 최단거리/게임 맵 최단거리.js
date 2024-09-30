0fad60cf0c714163c69467ee30a289ac97f2f38b
heeeete <101648575+heeeete@users.noreply.github.com>
2024-10-01 08:48:26 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 32.46 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,30 +1,70 @@
+
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
 function solution(maps) {
-    var answer = 0;
-    const dir = [[-1, 0], [0, 1], [1, 0], [0, -1]];
-    const q = [[0,0]];
-    let idx = 0;
-    
-    while(q.length !== idx){
-        const len = q.length;
-        for (let i = 0; i < len; i++){
-            let [y,x] = q.shift();
-            // let [y,x] = q[idx++];
-            maps[y][x] = 0;
-            for (let j = 0; j < 4; j ++){
-                const [dy, dx] = dir[j];
-                const [my, mx] = [y + dy, x + dx];
-                if (my === maps.length - 1 && mx === maps[0].length) return answer + 1
-                if (my >= 0 && mx >= 0 
-                    && my < maps.length 
-                    && mx < maps[0].length
-                    && maps[my][mx] === 1) {
-                    maps[my][mx] = 0
-                    q.push([my, mx]);
+    // 1. 남동북서 기준
+    const dy = [1,0,-1,0];
+    const dx = [0,1,0,-1];
+    const row = maps.length;
+    const col = maps[0].length;
+
+    // 2.
+    const visitCount = [...maps].map((r) => r.map((c) => 1));
+
+    // 3.
+    const queue = [[0,0]];  //시작점
+
+    // 4.
+    while(queue.length) {
+        const [y, x] = queue.shift();
+
+        // 5.
+        for(let i = 0; i < 4; i++ ) {
+            const ny = y + dy[i];
+            const nx = x + dx[i];
+
+            // 6.
+            if(ny >= 0 && nx >= 0 && ny < row && nx < col) {
+                // 7.
+                if(maps[ny][nx] === 1 && visitCount[ny][nx] === 1) {
+                    visitCount[ny][nx] = visitCount[y][x] + 1;
+                    queue.push([ny,nx]);
                 }
             }
         }
-        answer++;
     }
-    
-    return -1;
+
+    return visitCount[row - 1][col - 1] === 1 ? -1 : visitCount[row - 1][col - 1];    
 }
