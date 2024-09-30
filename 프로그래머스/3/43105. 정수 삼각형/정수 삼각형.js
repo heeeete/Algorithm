f9127e3ed0a1e3f4d4c6fa0dba71c32d21c26f34
heeeete <101648575+heeeete@users.noreply.github.com>
2024-09-30 21:06:57 +0900

    [level 3] Title: 정수 삼각형, Time: 4.29 ms, Memory: 40.8 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,27 +1,9 @@
 function solution(triangle) {
-	if (triangle.length === 1) return triangle[0][0];
-	const yLen = triangle.length;
 
-	for (let i = 1; i < yLen; i++) {
-		const xLen = triangle[i].length - 1;
-		for (let j = 0; j < xLen + 1; j++) {
-			if (i === 1) {
-				triangle[i][j] = triangle[0][0] + triangle[i][j];
-			} else {
-				if (j === 0) {
-					triangle[i][j] = triangle[i - 1][j] + triangle[i][j];
-				} else if (j === xLen) {
-					triangle[i][j] = triangle[i - 1][j - 1] + triangle[i][xLen];
-				} else {
-					const max =
-						triangle[i - 1][j] > triangle[i - 1][j - 1]
-							? triangle[i - 1][j]
-							: triangle[i - 1][j - 1];
-					triangle[i][j] = max + triangle[i][j];
-				}
-			}
-		}
-	}
-	const max = Math.max(...triangle[triangle.length - 1]);
-	return max;
+    for (let i = triangle.length - 2; i >= 0; i--){
+        for (let j = 0; j < triangle[i].length; j++){
+            triangle[i][j] += Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]);
+        }
+    }
+    return triangle[0][0]
 }
