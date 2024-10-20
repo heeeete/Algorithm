aa9e969c2f8132fe3e1718c4763493ee06dd5869
heeeete <101648575+heeeete@users.noreply.github.com>
2024-10-20 14:29:27 +0900

    [level 3] Title: 단속카메라, Time: 2.71 ms, Memory: 36.1 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.js
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.js
@@ -1,10 +1,7 @@
 function solution(routes) {
     var answer = 1;
-    
-    routes.map(e => e.sort((a,b) => a - b));
     routes.sort((a,b) => a[0] - b[0]);
     
-    console.log(routes)
     let target;
     for (let i = routes.length - 1; i >= 0; i--){
         if (!target) {
